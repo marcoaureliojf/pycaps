ec71eb5d93281ebc28410909b13ab4df83619243
Franco Zanardi <francozanardi.97@gmail.com>
2025-05-31 12:15:47 -0300

    refactor & small fix: improve positions algorithm -> now word positions in lines are not changed in the different line states if the line is not stable (has clips for the same word with different sizes).

--- src/pycaps/layout/positions_calculator.py
+++ src/pycaps/layout/positions_calculator.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from ..tagger.models import ElementState
 from ..models import SubtitleLayoutOptions
 from ..utils.alignment_utils import AlignmentUtils
@@ -14,37 +14,82 @@ class PositionsCalculator:
         """
         for segment in document.segments:
             self._calculate_words_positions(segment.lines, video_width, video_height)
-        
+    
+    # I'm not sure if this is the best approach.
+    # Each word slot will be calculated per line state.
+    # So, for example, if in a line already narrated the word slots are shorter than when the line is being narrated,
+    # It will cause that the words have different positions for the same line in different states.
+    # Then a line "Hello world" can be rendered as "Hello   world" or "Hello world" in different states.
+    # The adventage of this approach is that line size as a whole can change in different states (useful for .line-being-narrated class)
     def _calculate_words_positions(self, lines: List[Line], video_width: int, video_height: int) -> None:
         y = self._calculate_base_y_position(lines, video_height)
-        line_states = ElementState.get_all_line_states()
 
         for line in lines:
-            for state in line_states:
-                line_width = 0
-                x_gaps = []
-                for word in line.words:
-                    max_clip_width = 0
-                    for clip in word.clips:
-                        if state in clip.states:
-                            max_clip_width = max(max_clip_width, clip.layout.size.width)
-                    line_width += max_clip_width + self._options.word_spacing
-                    x_gaps.append(max_clip_width + self._options.word_spacing)
-                
-                start_x_for_line = (video_width - line_width) // 2
-                slot_x = start_x_for_line
-                for i, word in enumerate(line.words):
-                    slot_width = x_gaps[i]
-                    for clip in word.clips:
-                        if state in clip.states:
-                            # the clip is located in the center of the slot
-                            clip.layout.position.x = slot_x + (slot_width - clip.layout.size.width) // 2
-                            clip.layout.position.y = y + (line.max_layout.size.height - clip.layout.size.height) // 2
-                            clip.image_clip = clip.image_clip.set_position((clip.layout.position.x, clip.layout.position.y))
-
-                    slot_x += slot_width
+            if self._is_stable_line(line):
+                for state in ElementState.get_all_line_states():
+                    words_width = self._get_words_width_for_line_state(line, state)
+                    if words_width is None:
+                        continue
+                    self._set_clip_positions(line, words_width, y, video_width, state)
+            else:
+                word_widths = [word.max_layout.size.width for word in line.words]
+                self._set_clip_positions(line, word_widths, y, video_width)
 
             y += line.max_layout.size.height
+
+    def _set_clip_positions(self, line: Line, words_width: List[int], y: int, video_width: int, state: Optional[ElementState] = None) -> None:
+        line_width = sum(words_width) + (len(words_width) - 1) * self._options.word_spacing
+        start_x_for_line = (video_width - line_width) // 2
+        slot_x = start_x_for_line
+        for i, word in enumerate(line.words):
+            slot_width = words_width[i]
+            for clip in word.clips:
+                if state is None or state in clip.states:
+                    # the clip is located in the center of the slot
+                    clip.layout.position.x = slot_x + (slot_width - clip.layout.size.width) // 2
+                    clip.layout.position.y = y + (line.max_layout.size.height - clip.layout.size.height) // 2
+                    clip.image_clip = clip.image_clip.set_position((clip.layout.position.x, clip.layout.position.y))
+
+            slot_x += slot_width + self._options.word_spacing
+
+    def _is_stable_line(self, line: Line) -> bool:
+        '''
+        Returns True if the line is stable, False otherwise.
+        A stable line is a line where all the clips for each word for a line state have the same width.
+        For example,
+        line state: "being narrated"
+        if the word "hello" has a clip with width 100px when being narrated and 50px when not being narrated,
+        the line is not stable.
+        '''
+        line_states_that_support_words_in_multiple_states = [ElementState.LINE_BEING_NARRATED]
+        for state in line_states_that_support_words_in_multiple_states:
+            for word in line.words:
+                last_clip_width = None
+                for clip in word.clips:
+                    if state in clip.states:
+                        if last_clip_width is None:
+                            last_clip_width = clip.layout.size.width
+                        elif last_clip_width != clip.layout.size.width:
+                            return False
+                
+        return True
+
+    def _get_words_width_for_line_state(self, line: Line, state: ElementState) -> Optional[List[int]]:
+        '''
+        Returns the width of the words in the line in a specific state.
+        If there is a word with no clips for the given line state, returns None.
+        '''
+        words_width = []
+        for word in line.words:
+            max_clip_width = 0
+            for clip in word.clips:
+                if state in clip.states:
+                    max_clip_width = max(max_clip_width, clip.layout.size.width)
+            if max_clip_width == 0:
+                return None
+            words_width.append(max_clip_width)
+        return words_width
+
 
     def _calculate_base_y_position(self, lines: List[Line], video_height: int) -> float:
         """Calculates the base Y position for the subtitle block."""
