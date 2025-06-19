adeb0ec190788ea33223c26d478cae2b7e3a936c
Franco Zanardi <francozanardi.97@gmail.com>
2025-06-19 11:38:47 -0300

    peformance: slight improvement. Avoid using word index as key if is not needed (naive approach)

--- src/pycaps/renderer/rendered_image_cache.py
+++ src/pycaps/renderer/rendered_image_cache.py
@@ -1,4 +1,5 @@
-from typing import Optional, List, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
+import re
 
 if TYPE_CHECKING:
     from PIL.Image import Image
@@ -7,6 +8,7 @@ class RenderedImageCache:
     def __init__(self, css_content: str):
         self._css_content = css_content
         self._cache = {}
+        self._can_ignore_word_indexes = not self.__css_uses_position_dependent_backgrounds(css_content)
 
     def has(self, index: int, text: str, css_classes: str) -> bool:
         key = self.__build_key(index, text, css_classes)
@@ -25,5 +27,28 @@ class RenderedImageCache:
         self._cache[key] = image
 
     def __build_key(self, index: int, text: str, css_classes: str) -> str:
+        if self._can_ignore_word_indexes:
+            index = -1
         used_css_classes = [c for c in css_classes.split() if c in self._css_content]
         return f"word:{text}|index:{index}|css_classes:{','.join(used_css_classes)}"
+
+    def __css_uses_position_dependent_backgrounds(self, css_content: str) -> bool:
+        css_str = css_content.lower()
+
+        # Removing comments 
+        css_str = re.sub(r'/\*.*?\*/', '', css_str, flags=re.DOTALL)
+
+        # We search for patterns that creates a dependency for the word position
+        # (if the line has a gradient, the same word in different positions are different images)
+        # This is a naive approach, since we are not checking it for line classes
+        # We could improve it, doing a calculation using JS in open_line()
+        # However, this approach should be good enough for now.
+        patterns = [
+            r'linear-gradient\s*\(',
+            r'radial-gradient\s*\(',
+            r'conic-gradient\s*\(',
+            r'repeating-(linear|radial|conic)-gradient\s*\(',
+            r'background(-image)?\s*:\s*url\s*\(',
+        ]
+
+        return any(re.search(pattern, css_str) for pattern in patterns)
