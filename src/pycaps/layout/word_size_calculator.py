86a7ee56b9c305a7d4491d0f311519f96d2f7610
Franco Zanardi <francozanardi.97@gmail.com>
2025-06-30 11:31:36 -0300

    remove testing logs

--- src/pycaps/layout/word_size_calculator.py
+++ src/pycaps/layout/word_size_calculator.py
@@ -15,8 +15,5 @@ class WordSizeCalculator:
                     continue
                 max_width = max(max_width, w)
                 max_height = max(max_height, h)
-            if word.text.lower() == "i" or word.text.lower() == "am":
-                from pycaps.logger import logger
-                logger().debug(f"'{word.text}': width: {max_width}, height: {max_height}")
             word.max_layout.size.width = max_width
             word.max_layout.size.height = max_height
